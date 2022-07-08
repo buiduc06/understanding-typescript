f9fa99f9dd6259c10119b9baa201107062d4918a
Bùi Ngọc Đức <buiduc1998@gmail.com>
2022-07-08 16:45:46 +0700

    feat: next-gen-06-finished

--- basic-types/app.js
+++
@@ -1,8 +0,0 @@
-function generate_error(message, code) {
-    if (code === void 0) { code = 200; }
-    throw {
-        message: message,
-        code: code
-    };
-}
-generate_error("Internal errors");
---
+++ compiler-configuration/README.md
@@ -0,0 +1,17 @@
+## Install & start
+
+```
+npm install
+npm start
+```
+
+### commands
+
+- Khởi tạo tsconfig file
+  ```
+  tsc --init
+  ```
+- Build all files
+  ```
+  tcs -w
+  ```
